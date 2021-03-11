758dc4290e1d6f0e5d27dca603d724b616f000c6
Rainer <rainer.henkner@t-online.de>
2021-03-11 22:13:08 +0100

    1. Step Main

--- Crc2Hex/Crc2Hex.cpp
+++ Crc2Hex/Crc2Hex.cpp
@@ -1,20 +1,96 @@
-// Crc2Hex.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Programms.
-//
-
 #include <iostream>
+#include <string>
+#include <fstream>
+#include "lib.h"
 
-int main()
+using namespace std;
+
+#define PRG_VERSION     2
+#define SUB_VERSION     1
+#define SUB_SUB_VERSION 0
+
+#define FLASH_SIZE 0x100000
+#define OFFS_FLASH_START     56
+#define OFFS_FLASH_END       60
+#define OFFS_FLASH_GAP_START 64
+#define OFFS_FLASH_GAP_END   68
+#define OFFS_ADD32           72
+#define OFFS_CRC16           76
+
+void writeHelp(void);
+
+int main(int argc, char** argv)
 {
-    std::cout << "Hello World!\n";
+    int result = 1;
+    bool bQuiet = false;
+    bool bHelp = false;
+    string fileName = "";
+    string cfgFile = "";
+
+
+    cout << "CRC Builder for Intel-Hex-Files V" << to_string(PRG_VERSION);
+    cout << "." << to_string(SUB_VERSION) << "." << to_string(SUB_SUB_VERSION) << endl;
+    cout << "(c) 2021 AVENTICS GmbH - R&D Electronics Marine" << endl;
+
+    if (argc >= 2)
+    {   // es muss mindestens 1 Argument übergeben werden
+        for (int i = 1; i < argc; i++)
+        {
+            string cmd = UCase(argv[i]);
+            if (cmd == "QUIET")
+            {
+                bQuiet = true;
+            }
+            else if (cmd == "HELP")
+            {
+                bHelp = true;
+            }
+            else if (cmd.length() >= 3 && cmd.substr(0, 2) == "C:")
+            {
+                cfgFile = argv[i];
+                cfgFile = cfgFile.substr(2, cfgFile.length() - 2);
+            }
+            else
+            {
+                fileName = argv[i];
+            }
+        }
+        if (bHelp)
+        {
+            // Ausgabe des Hilfetextes
+            writeHelp();
+        }
+        else
+        {
+            /* File bearbeiten */
+            ifstream ifile;
+            ifile.open(fileName);
+            if (ifile)
+            {
+                
+            }
+            else
+            {
+                cout << "Error: file \"" << fileName.c_str() << "\" don't exist!" << endl;
+            }
+        }
+
+
+    }
+    else
+    {
+        cout << "Error: please call \"Crc2Hex.exe <filename.hex> [\c:cfgfile] [quiet] [help]\"" << endl;
+    }
+    return result;
+
 }
 
-// Programm ausführen: STRG+F5 oder Menüeintrag "Debuggen" > "Starten ohne Debuggen starten"
-// Programm debuggen: F5 oder "Debuggen" > Menü "Debuggen starten"
-
-// Tipps für den Einstieg: 
-//   1. Verwenden Sie das Projektmappen-Explorer-Fenster zum Hinzufügen/Verwalten von Dateien.
-//   2. Verwenden Sie das Team Explorer-Fenster zum Herstellen einer Verbindung mit der Quellcodeverwaltung.
-//   3. Verwenden Sie das Ausgabefenster, um die Buildausgabe und andere Nachrichten anzuzeigen.
-//   4. Verwenden Sie das Fenster "Fehlerliste", um Fehler anzuzeigen.
-//   5. Wechseln Sie zu "Projekt" > "Neues Element hinzufügen", um neue Codedateien zu erstellen, bzw. zu "Projekt" > "Vorhandenes Element hinzufügen", um dem Projekt vorhandene Codedateien hinzuzufügen.
-//   6. Um dieses Projekt später erneut zu öffnen, wechseln Sie zu "Datei" > "Öffnen" > "Projekt", und wählen Sie die SLN-Datei aus.
+void writeHelp(void)
+{
+    // Ausgabe der Helpinformationen
+    cout << endl;
+    cout << "call \"Crc2Hex.exe <filename.hex> [\c:cfgfile] [quiet] [help]\"" << endl << endl;
+    cout << "[\c:cfgfile] configuration file" << endl;
+    cout << "[quiet] no output of additional information like checksum and program length" << endl;
+    cout << "[help]  output of this help" << endl;
+}
